1a06b2b60cf0d641f0b7d8a91542bed8c00d7e65
Andrei Makarevich <i@makarevich-pro.ru>
2024-02-14 16:49:23 +0300

    hometask1

--- hometaskk1/src/main/java/Main.java
+++ hometaskk1/src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String s1 = "Hello";
         String s2 = "World";
-        System.out.println(s1+s2);
+        System.out.println(s1+" "+ s2);
     }
 }
