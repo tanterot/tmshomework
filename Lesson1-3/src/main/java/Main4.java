92672fe28e4fac4119e44d694f806460ee2f4e14
Andrei Makarevich <i@makarevich-pro.ru>
2024-02-28 11:00:20 +0300

    Lessons1-4

--- Lesson1-3/src/main/java/Main4.java
+++ Lesson1-3/src/main/java/Main4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main5 {
+public class Main4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
