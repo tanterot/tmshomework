33d9ca91f331c819e207bf4716e5985eec8edde7
Andrei Makarevich <i@makarevich-pro.ru>
2024-03-15 15:33:01 +0300

    Lessons11_Strings

--- Lesson11_String/src/main/java/TestMain.java
+++ Lesson11_String/src/main/java/TestMain.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TestMain {
     public static void main(String[] args) {
@@ -11,18 +9,6 @@ public class TestMain {
         System.out.println(Arrays.toString(split).length());     //вывод ее длинны
         System.out.println(test.substring(5));        //подстрока с позиции 5, метод substring
         System.out.println(new StringBuilder(test).reverse()); //зеркальная строка в StringBuilder метод revers
-        //-----------
-        String template = "Hi %s you are %s years old today. Happy birthday!!! ";
-        String name = "Marko Polo";
-        int age = 30;
-        String newFormat = String.format(template, name, age);
-        System.out.println(newFormat);
-        Pattern pattern = Pattern.compile("([A-Za-z]+)\\s([A-Za-z]+)");
-        Matcher matcher = pattern.matcher(newFormat);
-        if (matcher.find()) {
-            System.out.println("Строка соответствует паттерну");
-        } else {
-            System.out.println("Строка не соответствует паттерну");
-        }
+
     }
 }
