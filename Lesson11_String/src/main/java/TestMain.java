3add48059c23ed6c922aab18752776a357dfb935
Andrei Makarevich <i@makarevich-pro.ru>
2024-03-18 14:40:57 +0300

    Lessons11 Fix

--- Lesson11_String/src/main/java/TestMain.java
+++ Lesson11_String/src/main/java/TestMain.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class TestMain {
     public static void main(String[] args) {
-        String test = "Hello world. I'm Andrei. Nice to mit you.";
+        String test = "Hello world! I'm Andrei! Nice to mit you.";
         System.out.println(test.length());                          //длинна сторки
-        String[] split = test.split("/.");                   //разбил строку медод split, разделитель --> . <--;
+        String[] split = test.split("!");                   //разбил строку медод split, разделитель --> . <--;
         System.out.println(Arrays.toString(split));               //вывод массива элементов
-        System.out.println(Arrays.toString(split).length());     //вывод ее длинны
+        System.out.println(split.length);                         //вывод ее длинны
         System.out.println(test.substring(5));        //подстрока с позиции 5, метод substring
         System.out.println(new StringBuilder(test).reverse()); //зеркальная строка в StringBuilder метод revers
 
