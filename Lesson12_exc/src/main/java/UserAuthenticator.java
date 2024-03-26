3ee37d4e8685d1acf76301dfb1dda8552287b7d0
Andrei Makarevich <i@makarevich-pro.ru>
2024-03-26 19:22:29 +0300

    Lessons12 excfix

--- Lesson12_exc/src/main/java/UserAuthenticator.java
+++ Lesson12_exc/src/main/java/UserAuthenticator.java
@@ -1,8 +1,9 @@
 public class UserAuthenticator {
     public static boolean validateCredentials(String login, String password, String confirmPassword){
         try {
-            ValidationService.validateLogin(login);
-            ValidationService.validatePassword(password, confirmPassword);
+            ValidationService validationService = new ValidationService();
+            validationService.validateLogin(login);
+            validationService.validatePassword(password, confirmPassword);
             return true;
         } catch (WrongLoginException | WrongPasswordException exc) {
             System.out.println(exc.getMessage());
@@ -12,14 +13,14 @@ public class UserAuthenticator {
 }
 
 class ValidationService {
-    public static void validateLogin(String login) throws WrongLoginException {
+    public void validateLogin(String login) throws WrongLoginException {
         if (login.length() >= 20 || login.contains(" ")) {
             throw new WrongLoginException("invalid login");
 
         }
     }
 
-    public static void validatePassword(String password, String confirmPassword) throws WrongPasswordException {
+    public void validatePassword(String password, String confirmPassword) throws WrongPasswordException {
         if (password.length() >= 20
                 || password.contains(" ")
                 || !password.matches(".*\\d.*")
