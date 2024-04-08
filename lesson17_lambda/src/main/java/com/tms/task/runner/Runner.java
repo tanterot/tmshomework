9d7ea0cd10c7e44b9f4756727625f8eaf5c0e361
andreimakarevich <i@makarevich-pro.ru>
2024-04-08 07:41:00 +0300

    Lesson 17 fix

--- lesson17_lambda/src/main/java/com/tms/task/runner/Runner.java
+++ lesson17_lambda/src/main/java/com/tms/task/runner/Runner.java
@@ -2,14 +2,15 @@ package com.tms.task.runner;
 
 import com.tms.task.method.Converter;
 import com.tms.task.method.MethodReplace;
+import com.tms.task.util.Constant;
 
 public class Runner {
     public static void main(String[] args) {
 //------------------------------------------------------------------------- task1
-        Double apply = Converter.parseBYNtoUSD.apply("615");
+        Double apply = Converter.parseBYNtoUSD.apply(Constant.BYN);
         System.out.println(apply);
 //------------------------------------------------------------------------- task2
-        Converter.parseConsumerBYNtoUSD.accept("1000");
+        Converter.parseConsumerBYNtoUSD.accept(Constant.BYN);
 //------------------------------------------------------------------------- task3
         String s = MethodReplace.replaceString.get();
         System.out.println(s);
--- lesson17_lambda/src/main/java/com/tms/task/util/Constant.java
+++ lesson17_lambda/src/main/java/com/tms/task/util/Constant.java
@@ -2,4 +2,5 @@ package com.tms.task.util;
 
 public interface Constant {
     public double USD = 3.28;
+    public String BYN = "1000";
 }
